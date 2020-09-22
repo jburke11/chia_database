ce7cdd041a5c615ace09c934ea9f98e9c947a395
jburke11 <burkej24@msu.edu>
2020-09-22 15:02:48 -0400

    loaded genes into database'

---
+++ add_genes (1).py
@@ -0,0 +1,15 @@
+import pymongo
+# get the client
+client = pymongo.MongoClient()
+# connect to the genes collection in the chia database
+db = client.chia
+collection = db.genes
+
+fp = open( "/genes.txt" , "r" )
+count = 0
+for line in fp:
+    data = {"transcript_id": line[1:].rstrip()}
+    collection.insert_one(data)
+    count += 1
+    print("Gene inserted")
+print(59062 == count)
